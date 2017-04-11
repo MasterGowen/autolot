11598cda6da3d1d97e642508664582587d9b501d
MasterGowen <mastergowen@gmail.com>
2017-04-12 01:58:37 +0500

    simple config

--- autolot_backend.py
+++ autolot_backend.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask.ext.cqlalchemy import CQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config_dev')
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "test"
+app.config['CASSANDRA_SETUP_KWARGS'] = {'protocol_version': 3}
 
 
 db = CQLAlchemy(app)
--- config_dev.py
+++ config_dev.py
@@ -1,3 +1,3 @@
-CASSANDRA_HOSTS = ['127.0.0.1']
-CASSANDRA_KEYSPACE = "test"
-CASSANDRA_SETUP_KWARGS = {'protocol_version': 3}
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "test"
+app.config['CASSANDRA_SETUP_KWARGS'] = {'protocol_version': 3}
