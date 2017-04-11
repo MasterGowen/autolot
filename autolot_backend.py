42ffde5cc15281c634b9a33265c56fd7b2eefaee
MasterGowen <mastergowen@gmail.com>
2017-04-12 02:04:45 +0500

    add keyspace

--- autolot_backend.py
+++ autolot_backend.py
@@ -3,6 +3,7 @@ from flask.ext.cqlalchemy import CQLAlchemy
 
 app = Flask(__name__)
 app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "test"
 app.config['CASSANDRA_SETUP_KWARGS'] = {'protocol_version': 3}
 
 
@@ -10,6 +11,7 @@ db = CQLAlchemy(app)
 
 
 class User(db.Model):
+    __keyspace__ = 'test'
     name = db.columns.Text(primary_key=True)
 
 
